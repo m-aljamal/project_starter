94edb19088df04dd10e9d6c307ee301c540fc5c7
m-aljamal <mohammadjamol@gmail.com>
2020-06-15 08:22:07 +0300

    complete the auth

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,5 @@
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
 
 const requestLogger = (req, res, next) => {
   logger.info("Method", req.method);
@@ -14,10 +15,34 @@ const unknwnEndPoint = (req, res) => {
 
 const errorHandler = (error, req, res, next) => {
   logger.error(error.message);
+
+  if (error.name === "existingUser")
+    res.status(400).json({ error: error.message });
+  else if (error.name === "JsonWebTokenError")
+    res.status(401).json({ error: error.message });
+  else res.status(400).json({ error: error.message });
+
   next(error);
 };
+
+const getUsetFromToken = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    return res.status(401).json({ error: "No token, authoriztion denied" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_CREATE_USER);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ msg: "invaild token" });
+  }
+};
+
 module.exports = {
+  getUsetFromToken,
   requestLogger,
   errorHandler,
   unknwnEndPoint,
+  getUsetFromToken,
 };
